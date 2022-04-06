c2cc32f611d78d53e3fabb1ed381abf5c6cfc9a3
Ericprprpr <eric20000615@gmail.com>
2022-04-06 14:32:07 +0800

    9 21 21 state spec

--- environment.py
+++ environment.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import abc
 import tensorflow as tf
+import tensorflow_probability as tfp
 import networkx as nx
 import numpy as np
 import sfcsim
@@ -13,6 +14,22 @@ from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
 from tf_agents.environments import utils
 from tf_agents.specs import array_spec
+from tf_agents.specs import tensor_spec
+from tf_agents.networks import network, q_network, sequential
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.utils import common
+
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.policies import py_policy
+from tf_agents.policies import random_py_policy
+from tf_agents.policies import scripted_py_policy
+
+from tf_agents.policies import tf_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.policies import actor_policy
+from tf_agents.policies import q_policy
+from tf_agents.policies import greedy_policy
 from tf_agents.environments import wrappers
 from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
@@ -21,6 +38,8 @@ discount = 1.0
 
 fail_reward = -1
 success_reward = 1
+
+batch_size = 64
 
 
 class NFVEnv(py_environment.PyEnvironment):
@@ -47,7 +66,7 @@ class NFVEnv(py_environment.PyEnvironment):
         self._node_proc = None
 
         self._action_spec = array_spec.BoundedArraySpec(
-            shape=(), dtype=np.int32, minimum=1, maximum=self._node_num, name='action'
+            shape=(), dtype=np.int32, minimum=0, maximum=self._node_num-1, name='action'
         )
         self._observation_spec = array_spec.BoundedArraySpec(
             shape=(8+self._node_resource_attr_num, self._node_num, self._node_num), dtype=np.float32,
@@ -167,7 +186,7 @@ class NFVEnv(py_environment.PyEnvironment):
         else:
             self._node_last = self._node_proc
 
-        self._node_proc = self.network.get_node(self.network_matrix.get_node_list()[action-1])
+        self._node_proc = self.network.get_node(self.network_matrix.get_node_list()[action])
 
         path = nx.shortest_path(self.network.G, source=self._node_last, target=self._node_proc, weight='delay')
         delay = nx.shortest_path_length(self.network.G, source=self._node_last, target=self._node_proc, weight='delay')
@@ -249,8 +268,92 @@ class NFVEnv(py_environment.PyEnvironment):
 
 
 if __name__ == '__main__':
-    env = NFVEnv()
-    utils.validate_py_environment(env, episodes=5)
+    num_iterations = 20000  # @param {type:"integer"}
+
+    initial_collect_steps = 100  # @param {type:"integer"}
+    collect_steps_per_iteration = 1  # @param {type:"integer"}
+    replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+    batch_size = 64  # @param {type:"integer"}
+    learning_rate = 1e-3  # @param {type:"number"}
+    log_interval = 200  # @param {type:"integer"}
+
+    num_eval_episodes = 10  # @param {type:"integer"}
+    eval_interval = 1000  # @param {type:"integer"}
+
+    train_py_env = NFVEnv()
+    eval_py_env = NFVEnv()
+
+    train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+    eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+    fc_layer_params = (100, 50)
+    action_tensor_spec = tensor_spec.from_spec(train_env.action_spec())
+    num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+
+    # Define a helper function to create Dense layers configured with the right
+    # activation and kernel initializer.
+    def dense_layer(num_units):
+        return tf.keras.layers.Dense(
+            num_units,
+            activation=tf.keras.activations.relu,
+            kernel_initializer=tf.keras.initializers.RandomUniform(
+                minval=-0.05, maxval=0.05, seed=None))
+
+
+    # QNetwork consists of a sequence of Dense layers followed by a dense layer
+    # with `num_actions` units to generate one q_value per available action as
+    # its output.
+    dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+    q_values_layer = tf.keras.layers.Dense(
+        num_actions,
+        activation=None,
+        kernel_initializer=tf.keras.initializers.RandomUniform(
+            minval=-0.03, maxval=0.03),
+        bias_initializer=tf.keras.initializers.Constant(-0.2))
+    q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    train_step_counter = tf.Variable(0)
+
+    agent = dqn_agent.DqnAgent(
+        train_env.time_step_spec(),
+        train_env.action_spec(),
+        q_network=q_net,
+        optimizer=optimizer,
+        td_errors_loss_fn=common.element_wise_squared_loss,
+        train_step_counter=train_step_counter)
+
+    agent.initialize()
+
+    eval_policy = agent.policy
+    collect_policy = agent.collect_policy
+    random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                    train_env.action_spec())
+
+    def compute_avg_return(environment, policy, num_episodes=10):
+
+        total_return = 0.0
+        for _ in range(num_episodes):
+
+            time_step = environment.reset()
+            episode_return = 0.0
+
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = environment.step(action_step.action)
+                episode_return += time_step.reward
+            total_return += episode_return
+
+        avg_return = total_return / num_episodes
+        return avg_return.numpy()[0]
+
+    # See also the metrics module for standard implementations of different metrics.
+    # https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
+
+    compute_avg_return(eval_env, random_policy, num_eval_episodes)
 
 
 
